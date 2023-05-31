918ec8688bf52f247483498cf44c1f2c506d8c66
Sergey Kahnwald <vvintpann@gmail.com>
2023-06-01 01:13:04 +0300

    mvp

--- src/dev/testing.tsx
+++ src/dev/testing.tsx
@@ -2,20 +2,21 @@ import { autorun } from 'mobx';
 import { createObservable } from '../core/observable/lib.ts';
 import { useButtonControl } from 'storybox-react';
 
-const log: typeof console.log = (...args) => console.log('REACTIVE[DEBUG]', ...args)
+const log: typeof console.log = (...args) =>
+  console.log('REACTIVE[DEBUG]', ...args);
 
 const testCreateObservable = () => {
   const observable = createObservable('');
   log('observable.value', observable.value);
   observable.next('1');
-  observable.next(p => p+'1')
-  observable.value+='1';
+  observable.next((p) => p + '1');
+  observable.value += '1';
   autorun(() => {
-    log('AUTORUN observable.value >>', observable.value)
-  })
+    log('AUTORUN observable.value >>', observable.value);
+  });
   observable.next('2');
-  observable.next(p => p+'2')
-  observable.value+='2';
+  observable.next((p) => p + '2');
+  observable.value += '2';
 };
 
 export const Testing = () => {
