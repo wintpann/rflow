ca9a8563ac69b340e1e6c3098ad72d2dc10126fd
Sergey Kahnwald <vvintpann@gmail.com>
2023-06-04 22:11:24 +0300

    mvp

--- src/dev/expose-window.ts
+++ src/dev/expose-window.ts
@@ -2,12 +2,30 @@
 // @ts-nocheck
 import {
   createObservable,
-  Observable,
   createDerivation,
-} from '../core/observable/lib.ts';
+  from,
+  take,
+  pipe,
+  debounceTime,
+  interval,
+  distinctUntilChanged,
+  of,
+  map,
+  fromEvent,
+  future,
+} from '../core/index.ts';
 import { autorun } from 'mobx';
 
-window.Observable = Observable;
+window.autorun = autorun;
 window.createObservable = createObservable;
 window.createDerivation = createDerivation;
-window.autorun = autorun;
+window.from = from;
+window.take = take;
+window.pipe = pipe;
+window.debounceTime = debounceTime;
+window.interval = interval;
+window.distinctUntilChanged = distinctUntilChanged;
+window.of = of;
+window.map = map;
+window.fromEvent = fromEvent;
+window.future = future;
