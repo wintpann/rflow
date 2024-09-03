70c1e1f4041e68f88c90ab606aba5d39b685ce09
Sergei Kahnwald <vvintpann@gmail.com>
2024-09-03 19:07:25 +0300

    fast commit

--- src/finally/distinctUntilChanged/lib.spec.ts
+++ src/finally/distinctUntilChanged/lib.spec.ts
@@ -1,10 +1,69 @@
 import { of } from '../observable';
 import { distinctUntilChanged } from './lib.ts';
-import { observe, read } from '../test-utils.ts';
+import { observe } from '../test-utils.ts';
 import { scheduler } from '../scheduler';
 import { map } from '../map';
 
 describe('distinctUntilChanged', () => {
+  it('should work on unobserved access', () => {
+    const source = of(2);
+    const distincted = source.pipe(distinctUntilChanged());
+
+    expect(source()).toBe(2);
+    expect(distincted()).toBe(2);
+
+    source.next(4);
+    expect(source()).toBe(4);
+    expect(distincted()).toBe(4);
+  });
+
+  it('should work on observed access', () => {
+    const source = of(2);
+    const distincted = source.pipe(distinctUntilChanged());
+
+    const run1 = observe(distincted);
+    const run2 = observe(distincted);
+
+    source.next(3);
+    source.next(4);
+    scheduler.flush();
+    expect(run1.updates.current).toStrictEqual([4]);
+    expect(run2.updates.current).toStrictEqual([4]);
+
+    run1.dispose();
+    run2.dispose();
+  });
+
+  it('should calculate value & not schedule update if observed and source hasScheduledUpdate', () => {
+    const source = of(1);
+    const distincted = source.pipe(distinctUntilChanged());
+    const distinctedObserve = jest.fn();
+
+    const dispose1 = distincted.observe(distinctedObserve);
+    source.next(2);
+    expect(distincted()).toBe(2);
+    scheduler.flush();
+    expect(distinctedObserve).not.toHaveBeenCalled();
+
+    dispose1();
+  });
+
+  it('should calculate value & not schedule update on BO cause source could have changed & updated', () => {
+    const source = of(1);
+    const distincted = source.pipe(distinctUntilChanged());
+    const distinctedObserve = jest.fn();
+
+    source.next(2);
+    source.next(3);
+    scheduler.flush();
+    const dispose1 = distincted.observe(distinctedObserve);
+    expect(distincted()).toBe(3);
+    scheduler.flush();
+    expect(distinctedObserve).not.toHaveBeenCalled();
+
+    dispose1();
+  });
+
   it('should work with default shallow comparer', () => {
     const source = of([1]);
     const distincted = source.pipe(distinctUntilChanged());
@@ -104,45 +163,6 @@ describe('distinctUntilChanged', () => {
     scheduler.flush();
 
     expect(run1.updates.current).toStrictEqual([[1]]);
-    run1.dispose();
-  });
-
-  it('should force calculating value if observed but source has coming update', () => {
-    const source = of([1]);
-    const distincted = source.pipe(distinctUntilChanged());
-
-    const run1 = observe(distincted);
-    source.next([2]);
-    expect(distincted()).toStrictEqual([2]);
-    scheduler.flush();
-
-    run1.dispose();
-  });
-
-  it('should not schedule update on read with coming update from source', () => {
-    const source = of([1]);
-    const distincted = source.pipe(distinctUntilChanged());
-    const distinctedObserve = jest.fn();
-
-    source.next([2]);
-    const dispose1 = distincted.observe(distinctedObserve);
-    read(distincted);
-    scheduler.flush();
-
-    expect(distinctedObserve).not.toHaveBeenCalled();
-    dispose1();
-  });
-
-  it('should get actual value after subscription when source has changed & updated', () => {
-    const source = of([1]);
-    const distincted = source.pipe(distinctUntilChanged());
-
-    source.next([2]);
-    scheduler.flush();
-    const run1 = observe(distincted);
-    expect(distincted()).toStrictEqual([2]);
-    scheduler.flush();
-
     run1.dispose();
   });
 });
--- src/finally/distinctUntilChanged/lib.ts
+++ src/finally/distinctUntilChanged/lib.ts
@@ -1,11 +1,11 @@
-import { shallowEqual } from 'shallow-equal';
+import equals from 'fast-deep-equal/es6';
 import { EqualsComparer } from './typings.ts';
 import { observable, Observable, introspect } from '../observable';
 
 export const distinctUntilChanged =
   <T>(comparer?: EqualsComparer<T>) =>
   (source: Observable<T, NonNullable<unknown>>): Observable<T> => {
-    const compare = (comparer || shallowEqual) as EqualsComparer<T>;
+    const compare = comparer || equals;
     let lastValue = source();
 
     return observable(lastValue).create({
@@ -13,7 +13,8 @@ export const distinctUntilChanged =
         parent: source,
         onRead: ({ next, isObserved }) => {
           if (!isObserved() || introspect.hasUpdates(source)) {
-            next(source(), { scheduleUpdate: false });
+            lastValue = source();
+            next(lastValue, { scheduleUpdate: false });
           }
         },
         onBecomesObserved: ({ next }) => {
